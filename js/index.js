4198343e6926e15c27ce8aff71bdc5eeb043112f
emon-hossain1001 <mrx00001001@gmail.com>
2024-12-08 10:50:45 +0600

    complete the donation and history btn functionalities

--- js/index.js
+++ js/index.js
@@ -1,1 +1,26 @@
-console.log('connected');
+// History Button Functionalities
+document.getElementById('history-btn').addEventListener('click', ()=>{
+    const historyBtn = document.getElementById('history-btn');
+    const donationBtn = document.getElementById('donation-btn');
+    // Show history section
+    const historySection = document.getElementById('history-section');
+    historySection.classList.remove("hidden");
+    historyBtn.classList.add("bg-[rgb(180,244,97)]")
+    // Remove donation section
+    const cardSection = document.getElementById('card-section');
+    cardSection.classList.add("hidden");
+    donationBtn.classList.remove("bg-[rgb(180,244,97)]");
+})
+// Donation Button Functionalities
+document.getElementById('donation-btn').addEventListener('click', ()=>{
+    const historyBtn = document.getElementById('history-btn');
+    const donationBtn = document.getElementById('donation-btn');
+    // Show donation section
+    const cardSection = document.getElementById('card-section');
+    cardSection.classList.remove("hidden");
+    donationBtn.classList.add("bg-[rgb(180,244,97)]");
+    // Remove history section 
+    const historySection = document.getElementById('history-section');
+    historySection.classList.add("hidden");
+    historyBtn.classList.remove("bg-[rgb(180,244,97)]")
+})
